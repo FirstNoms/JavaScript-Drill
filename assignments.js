3da881693eaa7273ee4018618ac59d35d0a57973
Confidence Okere <okerenomso@gmail.com>
2021-11-22 01:11:45 +0100

    started some drills in javascript

---
+++ assignments.js
@@ -0,0 +1,4 @@
+let country = "Nigeria";
+let continent = "Africa";
+let population = 5000000;
+console.log("My country is "+ country + " the giant of " + continent + " with the population of "+ population + " people.")
---
+++ index.js
@@ -0,0 +1,4 @@
+let quality = "amazing";
+let value = 40 +8 + 23-10
+console.log(value);
+console.log(quality);
