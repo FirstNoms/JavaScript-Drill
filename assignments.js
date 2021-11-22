659d0c53a4f06322a1f646af631da104ca3fb206
Confidence Okere <okerenomso@gmail.com>
2021-11-22 02:00:27 +0100

    Practicing

--- assignments.js
+++ assignments.js
@@ -2,3 +2,20 @@ let country = "Nigeria";
 let continent = "Africa";
 let population = 5000000;
 console.log("My country is "+ country + " the giant of " + continent + " with the population of "+ population + " people.")
+
+//Data Types---------------------
+let isIsland = true;
+let language;
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
+
+
+
+/**1. Declare a variable called 'isIsland' and set its value according to your
+ country. The variable should hold a Boolean value. Also declare a variable
+ 'language', but don't assign it any value yet
+ 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+ to the consol
+ * */
