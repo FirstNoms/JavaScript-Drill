cbec824c2cd68e02d87449b97345b2328709e3c3
Confidence Okere <okerenomso@gmail.com>
2021-11-24 16:22:15 +0100

    Scratch Practice

--- assignments.js
+++ assignments.js
@@ -1,20 +1,13 @@
-//Switch Statements
-let favouriteColor = prompt("Whats your favourite color");
-switch (favouriteColor){
-    case "Blue":
-        console.log(`The Sky is Blue`);
-        break;
-    case "Red":
-        console.log(`Blood is Red`);
-}
+//Tenary Operator
+let population = Number(prompt("Whats your country's Population"))
+
+population >= 33000000 ? console.log(`Your country is over populated`):console.log(`Your country is under populated`);
 
 
-
-/**. Use a switch statement to log the following string for the given 'language':
- chinese or mandarin: 'MOST number of native speakers!'
- spanish: '2nd place in number of native speakers'
- english: '3rd place'
- hindi: 'Number 4'
- arabic: '5th most spoken language'
- for all other simply log 'Great language too :D
+/**If your country's population is greater than 33 million, use the ternary operator
+ to log a string like this to the console: 'Portugal's population is above average'.
+ Otherwise, simply log 'Portugal's population is below average'. Notice how only
+ one word changes between these two sentences!
+ 2. After checking the result, change the population temporarily to 13 and then
+ 130. See the different results, and set the population back to original
  * */
