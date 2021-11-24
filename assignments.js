5fd7f43e3cfa943d53caef92fcdcdb217c289d14
Confidence Okere <okerenomso@gmail.com>
2021-11-24 16:53:22 +0100

    Scratch Practice

--- assignments.js
+++ assignments.js
@@ -1,13 +1,26 @@
-//Tenary Operator
-let population = Number(prompt("Whats your country's Population"))
+//DRILL
+let bill= 275;
+let bill2 = 40;
+let bill3 = 430;
+let tip = bill3 <= 300 && bill3 >=50 ? bill3 * 0.5 : bill3 * 0.2;
+console.log(`Your bill is ${bill3} and the tip is ${tip} and total amount is ${tip + bill3}`)
 
-population >= 33000000 ? console.log(`Your country is over populated`):console.log(`Your country is under populated`);
 
-
-/**If your country's population is greater than 33 million, use the ternary operator
- to log a string like this to the console: 'Portugal's population is above average'.
- Otherwise, simply log 'Portugal's population is below average'. Notice how only
- one word changes between these two sentences!
- 2. After checking the result, change the population temporarily to 13 and then
- 130. See the different results, and set the population back to original
+/**Steven wants to build a very simple tip calculator for whenever he goes eating in a
+ restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+ 300. If the value is different, the tip is 20%.
+ Your tasks:
+ 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+ this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+ start with an if/else statement, and then try to convert it to a ternary
+ operator!)
+ 2. Print a string to the console containing the bill value, the tip, and the final value
+ (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+ 316.25”
+ Test data:
+ § Data 1: Test for bill values 275, 40 and 430
+ Hints:
+ § To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+ § Value X is between 50 and 300, if it's >= 50 && <= 300 �
+ GOOD LUCK
  * */
