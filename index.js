973073991104dd74a010d9a99c469a84ec187f20
Confidence Okere <okerenomso@gmail.com>
2021-11-30 17:51:11 +0100

    Scratch Practice

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ let quality = "amazing";
 let value = 40 +8 + 23-10
 console.log(value);
 console.log(quality);
+
+//Functions
+//No Parameter
+function logger(){
+    console.log(`I'm Nomso ChiNomso`)
+}
+logger();
+
+//With Parameter
+function describeCountry(country, population, capitalCity){
+    console.log(`${country} has ${population} people and its capital city is ${capitalCity}`)
+}
+
+describeCountry('Finland', 6000000, 'Helsinki');
+describeCountry("Nigeria", 10000000, "Abuja");
+describeCountry("France", 90000000, "Paris");
